7b84408dc0e58d2a2342c211e0be89361b807416
olorunwa-web <olorunwamoliki@gmail.com>
2025-02-15 14:54:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+
+
+
+let quoteBtn = document.querySelector('#quote-btn')
+
+
+async function fetchAdvice() {
+    try {
+        let response = await fetch('https://api.adviceslip.com/advice')
+        let data = await response.json();
+        console.log(data.slip.id);
+        document.getElementById('advice-tag').innerText = `ADVICE #${data.slip.id}`;
+        document.getElementById('word-tag').innerText = `"${data.slip.advice}"`
+    } catch (error) {
+        
+    }
+}
+
+quoteBtn.addEventListener('click', fetchAdvice)
+window.addEventListener('load', fetchAdvice)
